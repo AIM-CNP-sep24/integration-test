aa7636ee0a3296cf362cdf8b06198e3d2961b2e8
Fritz van Deventer <fritzvd@users.noreply.github.com>
2025-05-14 09:14:04 +0200

    init

---
+++ backend/integration-demo/src/main/java/org/example/integrationdemo/IntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.integrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationDemoApplication.class, args);
+    }
+
+}
